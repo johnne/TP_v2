36d5cbb9f0e40c1ccbb81734f9b4291c6cfb65d9
johnne <john.sundh@scilifelab.se>
2020-05-15 12:34:09 +0200

    Skip failed read files

--- demultiplex_wf/scripts/merge_failed.sh
+++ demultiplex_wf/scripts/merge_failed.sh
@@ -7,5 +7,5 @@ strip=$3
 for f in $(find $indir -name "*_r[1,2].fq.gz"); do
     n=$(basename $f | sed "s/$strip//g")
     mv $f ${f//.fq/.pass.fq}
-    cat ${f//.fq/.pass.fq} ${f//.fq/.fail.fq} > $outdir/$n
+    cat ${f//.fq/.pass.fq} > $outdir/$n
 done
