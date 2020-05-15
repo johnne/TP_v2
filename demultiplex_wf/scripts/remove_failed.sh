ba84d8633cc9ac9359de938f7bc0be4e87cb2072
johnne <john.sundh@scilifelab.se>
2020-05-15 22:04:49 +0200

    Revert remove_failed.sh script but clean unknown

--- demultiplex_wf/scripts/remove_failed.sh
+++ demultiplex_wf/scripts/remove_failed.sh
@@ -4,8 +4,11 @@ indir=$1
 outdir=$2
 strip=$3
 
+# Clean up the unknown files
+rm -f $indir/*_unknown_[r,i][1,2].f*q.gz
+
 for f in $(find $indir -name "*_r[1,2].fq.gz"); do
     n=$(basename $f | sed "s/$strip//g")
     mv $f ${f//.fq/.pass.fq}
-    cat ${f//.fq/.pass.fq} > $outdir/$n
+    cat ${f//.fq/.pass.fq} ${f//.fq/.fail.fq} > $outdir/$n
 done
